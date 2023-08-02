667663e43be53736a932717aa4433124528ec2d7
MarcThlt <mthes@tutanota.com>
2023-08-02 13:08:29 -0400

    Second commit

--- GradingSystem/src/gradingSystem/GradeLetter.java
+++ GradingSystem/src/gradingSystem/GradeLetter.java
@@ -4,6 +4,8 @@ package gradingSystem;
  * Enum representing the possible letter grades.
  */
 
+// This is my second commit that will show after I commit !
+
 public enum GradeLetter {
 	A, B, C, D, F
 }
